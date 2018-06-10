aa4e3beb76f2fb4f49c943867c761c0a61c10532
enzocr <equartinoz@ucenfotec.ac.cr>
2018-06-09 22:13:25 -0600

    Primera prueba

--- src/ac/cr/cenfotec/kata/test/ComponenteTest.java
+++ src/ac/cr/cenfotec/kata/test/ComponenteTest.java
@@ -1,5 +1,20 @@
 package ac.cr.cenfotec.kata.test;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import ac.cr.cenfotec.logica.Componente;
+
 public class ComponenteTest {
+
+	Componente comp = new Componente();
+
+	@Test
+	public void itIsANumber() {
+		assertEquals("11", comp.recibirNumero(11));
+		
+	}
+	
 
 }
--- src/ac/cr/cenfotec/logica/Componente.java
+++ src/ac/cr/cenfotec/logica/Componente.java
@@ -32,7 +32,7 @@ public class Componente {
 
 	@Override
 	public String toString() {
-		return " + numero + ";
+		return ""+numero+"";
 	}
 
 	public String recibirNumero(int pNumero) {
